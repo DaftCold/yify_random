906c92e0940c24f64d0be211e43cf0a70e1305e7
Louis Bokiau <louis.bokiau@gmail.com>
2021-06-03 20:46:15 +0200

    correction divers bug

--- App.js
+++ App.js
@@ -7,33 +7,40 @@ import { useEffect, useState } from 'react';
 export default function App() {
 
   const [isLoading, setLoading] = useState(true);
-  const [movieCount, setMovieCount] = useState([]);
+  const [movieCount, setMovieCount] = useState(0);
   const [randomMovie, setRandomMovie] = useState([]);
 
   useEffect(() => {
-    fetch('https://yts.mx/api/v2/list_movies.json')
-      .then((response) => response.json())
-      .then((json) => {
-        setMovieCount(json.data.movie_count);
-        getRandomMovie();
-      })
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
+    if(movieCount == 0){
+      fetch('https://yts.mx/api/v2/list_movies.json')
+        .then((response) => response.json())
+        .then((json) => {
+          setMovieCount(json.data.movie_count);
+          console.log(movieCount);
+        })
+        .then(getRandomMovie())
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    }
   }, []);
 
   const getRandomMovie = () => {
-    var id = Math.floor(Math.random() * movieCount);
-                fetch("https://yts.mx/api/v2/movie_details.json?movie_id=" + id + "&with_images=true")
-                .then((response) => response.json())
-                .then((json) =>{
-                  setRandomMovie(json.data.movie)
-                  console.log(randomMovie.medium_cover_image) 
-                })
-                .catch((error) => {
-                  console.error(error)
-                  //getRandomMovie();
-                })
-                .finally(() => setLoading(false));
+    if(movieCount !=0){
+      var id = Math.floor(Math.random() * movieCount);
+      var url = ("https://yts.mx/api/v2/movie_details.json?movie_id=" + id + "&with_images=true")
+      console.log(url);
+      fetch(url)
+      .then((response) => response.json())
+      .then((json) =>{
+        setRandomMovie(json.data.movie)
+        console.log(randomMovie) 
+      })
+      .catch((error) => {
+        console.error(error)
+        //getRandomMovie();
+      })
+      .finally(() => setLoading(false));
+    }
   } 
 
   return (
@@ -41,14 +48,15 @@ export default function App() {
        {isLoading ? <Text>Loading...</Text> :
 
       (<View> 
+        <Image source={randomMovie.medium_cover_image}></Image>
         <Text>titre : {randomMovie.title}</Text>       
         
-        {/* <TouchableOpacity style={styles.button}
+        { <TouchableOpacity style={styles.button}
               onPress={() => {
                 getRandomMovie();
               }}>
                 <Text style={styles.textButton}><FontAwesomeIcon icon={faFilm} size={40} color={"black"} /></Text>
-          </TouchableOpacity>  */}
+          </TouchableOpacity>}
         </View>)}
     </View>
   );
