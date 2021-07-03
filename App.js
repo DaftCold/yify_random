895729fb67a7fef9fbe716f80348a08f15954c9f
Louis <louis.bokiau@student.vinci.be>
2021-07-03 17:10:57 +0200

    ajout bouton lecture

--- App.js
+++ App.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import { Linking, ScrollView, Dimensions, Image, TouchableOpacity, StyleSheet, Text, View } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faFilm } from '@fortawesome/free-solid-svg-icons';
+import { faFilm, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 
 export default function App() {
 
   const [isLoading, setLoading] = useState(true);
-  const [movieCount, setMovieCount] = useState(0);
+  const [movieCount, setMovieCount] = useState(10000);
   const [randomMovie, setRandomMovie] = useState([]);
 
   const dimensions = Dimensions.get('window');
 
   useEffect(() => {
-    if(movieCount == 0){
+    if(movieCount == 10000){
       fetch('https://yts.mx/api/v2/list_movies.json')
         .then((response) => response.json())
         .then((json) => {
           setMovieCount(json.data.movie_count);
           console.log(movieCount);
         })
-        .then(getRandomMovie())
         .catch((error) => console.error(error))
-        .finally(() => setLoading(false));
+        .finally(() => {
+          setLoading(false);
+          getRandomMovie();
+        });
     }
   }, []);
 
@@ -53,11 +55,7 @@ export default function App() {
         <Image 
         style={styles.image}
         source={{uri : randomMovie.large_cover_image}}
-        onPress= { () => {
-          var torrent = randomMovie.torrents[0];
-          console.log(torrent);
-          Linking.openURL(torrent.url).catch(err => console.error("Couldn't load page", err));
-        }}/>
+        />
         <ScrollView style={styles.scrollView}>
         <Text style={styles.title}>{randomMovie.title}</Text>       
           <Text style={styles.description}>{randomMovie.description_full}</Text>
@@ -68,6 +66,14 @@ export default function App() {
               }}>
                 <Text style={styles.textButton}><FontAwesomeIcon icon={faFilm} size={40} color={"black"} /></Text>
           </TouchableOpacity>
+        <TouchableOpacity style={styles.button2}
+            onPress={() => {
+              var torrent = randomMovie.torrents[0];
+              console.log(torrent);
+              Linking.openURL(torrent.url).catch(err => console.error("Couldn't load page", err));
+            }}>
+              <Text style={styles.textButton}><FontAwesomeIcon icon={faPlay} size={40} color={"black"} /></Text>
+        </TouchableOpacity>
         </View>)}
     </ScrollView>
   );
@@ -97,6 +103,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     right: 30,
+    bottom: 30
+  },button2 : {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#6AC045",
+    marginTop : 30,
+    borderRadius:100,
+    height: 75,
+    width: 75,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    left: 30,
     bottom: 30
   },
   title : {
